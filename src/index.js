d7409d0b33f9d06c7a6b354d73b8c7cf4b6b457a
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-01-13 01:10:21 -0400

    chore: create code in the middleware and fix erros

--- src/index.js
+++ src/index.js
@@ -24,11 +24,46 @@ function checksExistsUserAccount(request, response, next) {
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(user.pro) {
+    return next();
+  }
+
+  if(user.todos.length === 10) {
+    return response.status(403).json({ error: "Go to account Pro"});
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const idIsUUID = validate(id);
+
+  if(!idIsUUID) {
+    return response.status(400).json({ error: ' Id not exist' });
+  } 
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({ error: ' Username not found' });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo) {
+    return response.status(404).json({ error: ' Todo not exist' });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
+  
 }
 
 function findUserById(request, response, next) {
